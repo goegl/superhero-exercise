90c139c22cbfb07c87adbc717317db708ccef905
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-02-20 11:17:42 +0100

    initial commit

---
+++ src/main/java/org/example/superheroexercise/SuperheroExerciseApplication.java
@@ -0,0 +1,13 @@
+package org.example.superheroexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperheroExerciseApplication.class, args);
+    }
+
+}
